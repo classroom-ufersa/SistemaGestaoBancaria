f8577d5ececc483ef921bdb549c66534b69fbd06
Paulo Henrique <138056303+Oluaphenrique@users.noreply.github.com>
2023-09-20 00:32:31 -0300

    agencia

--- agencia.c
+++ agencia.c
@@ -9,3 +9,45 @@ int contas;
 int horario;
 
 }Agenciabancaria;
+
+Agenciabancaria *criar_agencia(char *nome, int codigo, char* localizacao, int contas, int horario) {
+    
+      Agenciabancaria *criar_agencia = (Agenciabancaria*)malloc(sizeof(Agenciabancaria));
+    if(criar_agencia == NULL) {
+
+        printf("Variavel criar_agencia nao foi alocada dinamicamente! \n");
+        exit(1);
+    }
+    strcpy(agenciabancaria->nome, nome);   
+    agenciabancaria->codigo = codigo;
+    strcpy(agenciabancaria->localizacao, localizacao);  
+    agenciabancaria->contas = contas;
+    agenciabancaria->horario = horario;
+
+    return agenciabancaria;
+}
+
+
+void agencia_salva(Agenciabancaria *agenciabancaria)
+{
+
+    
+    FILE *fp = fopen("saida.txt", "a"); // Abre arquivo "a" (abre um arquivo no modo de inclusão)
+    if (fp == NULL)                    
+    {
+        printf("Erro ao abrir o arquivo"); 
+        exit(1);                           
+    }
+        
+    char *nome = agenciabancaria->nome;
+    int codigo = agenciabancaria->codigo;
+    char *localizacao = agenciabancaria->localizacao;
+    int contas = agenciabancaria->contas;
+    int horario = agenciabancaria->horario;
+    nome[strcspn(nome, "\n")] = '\0';
+    localizacao[strcspn(localizacao, "\n")] = '\0';
+
+    
+    fprintf(fp, " %s\t%s\t%lld\n", nome, cargo, funcionario->documento);
+    fclose(fp); 
+}
--- agencia.h
+++ agencia.h
@@ -5,3 +5,23 @@
 #include <string.h>
 #include <time.h>
 #include <ctype.h>
+
+typedef struct agenciabancaria Agenciabancaria;
+
+
+/*Função para cadastra uma nova agencia bancaria, alocando memoria*/
+
+Agenciabancaria *criar_agencia(char *nome, int codigo, char* localizacao, int contas, int horario)
+
+/*Função para salva arquivos*/
+
+void agencia_salva(Agenciabancaria *agenciabancaria)
+
+
+
+// função libera agencias vai liberar a memoria ocupada
+void libera_agencia(Agenciabancaria *agenciabancaria)
+{
+    
+    free(agenciabancaria);
+}
