7daff2a502d60d4956895fdee3952256973368e0
Diego Nascimento de Oliveira <138056676+diegonascimento2023@users.noreply.github.com>
2023-09-27 10:46:41 +0000

    adicionando a função lerDadosConta

--- contaBancaria.c
+++ contaBancaria.c
@@ -30,3 +30,40 @@ ContaBancaria *criarContaBancaria(int numeroConta, Agencia *agencia, char client
 
     return novaConta;
 }
+
+
+void lerDadosConta() {
+    FILE *arquivo = fopen("dados.txt", "r");
+    if (arquivo == NULL) {
+        printf("Erro: Não foi possível abrir o arquivo 'dados.txt'.\n");
+        exit(1);
+    }
+
+    char linha[256];
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        if (strncmp(linha, "Conta", 5) == 0) {
+            // Encontrou uma linha de conta, ler e exibir os campos
+            int numeroAgencia;
+            char cliente[50];
+            char dataAbertura[11];
+            float saldo;
+            char status[10];
+
+            if (sscanf(linha, "Conta\t%d\t%s\t%s\t%f\t%s", &numeroAgencia, cliente, dataAbertura, &saldo, status) == 5) {
+                printf("Número da Agência: %d\n", numeroAgencia);
+                printf("Cliente: %s\n", cliente);
+                printf("Data de Abertura: %s\n", dataAbertura);
+                printf("Saldo: %.2f\n", saldo);
+                printf("Status: %s\n", status);
+                printf("\n");
+            } else {
+                printf("Erro: Formato de linha de conta inválido.\n");
+            }
+        }
+    }
+
+    fclose(arquivo);
+}
+
+
--- main.c
+++ main.c
@@ -2,35 +2,12 @@
 
 int main(void){
 
-    Contabancaria * nome1 = criar_conta("joao", "15/09/2023", 1506.65, "ativo", 12711 );
-    printf(" %s\n", nome1->cliente);
-    printf(" %s\n", nome1->data);
-    printf(" %.2f\n", nome1->saldo);
-    printf(" %s\n", nome1->status);
-    printf(" %d\n", nome1->numero);
-
-    Contabancaria * nome2 = criar_conta("Almeida", "10/04/2021", 156.12, "ativo", 13833 );
-    printf(" %s\n", nome2->cliente);
-    printf(" %s\n", nome2->data);
-    printf(" %.2f\n", nome2->saldo);
-    printf(" %s\n", nome2->status);
-    printf(" %d\n", nome2->numero);
-
-    Agenciabancaria * agencia = criar_agencia("Banco", 1134, "São Bento", "09:30 ate 15:00" );
-    printf(" %s\n", agencia->nome);
-    printf(" %d\n", agencia->codigo);
-    printf(" %s\n", agencia->localizacao);
-    printf(" %s\n", agencia->horario);
-
-
-    agencia = cadastrar_cliente (nome2, agencia);
-    agencia = cadastrar_cliente (nome1, agencia);
     
-    printf(" %s\n", agencia->contas->conta->cliente);
-    printf(" %s\n", agencia->contas->prox->conta->cliente);
-    
-   
- /*  
+    lerDadosConta();
+
+ 
+
+    /*
     
  
     int op;
